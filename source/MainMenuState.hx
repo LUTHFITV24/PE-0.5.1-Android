b3fb20aa01dd00fcc945cb7ffab35aaed2823e51
LUTHFITV24 <102787531+LUTHFITV24@users.noreply.github.com>
2022-04-10 14:16:49 +0700

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -33,12 +33,12 @@ class MainMenuState extends MusicBeatState
 	private var camAchievement:FlxCamera;
 	
 	var optionShit:Array<String> = [
-		'story_mode',
+		  'story_mode',
 		'freeplay',
 		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
+		  #if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
-		#if !switch 'donate', #end
+		  #if !switch 'donate', #end
 		'options'
 	];
 
@@ -114,7 +114,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
 			// menuItem.screenCenter(X);
-			menuItem.x += 250;
+			menuItem.x += 270;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
